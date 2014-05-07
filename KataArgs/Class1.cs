0a5ac52f1c81dfa6d64b24303cc2b4051b585d85
Sven Sönnichsen <sven@sven-s.de>
2014-05-07 15:31:05 +0200

    Added project files

---
+++ KataArgs/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace KataArgs
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1,0);
+            
+        }
+
+    }
+}
